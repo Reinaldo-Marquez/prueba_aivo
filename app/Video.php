d6a441b7d75d76f034585535efc2a53c64f57883
Reinaldo-Marquez <rmarquez1505@gmail.com>
2020-05-28 14:47:54 -0300

    completed

--- app/Video.php
+++ app/Video.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     protected $guarded = [];
+
     public $timestamps = false;
+
     public $incrementing = false;
+
     protected $keyType = 'string';
+
     public function scopeTitle($query, $title)
     {
         if ($title) {
